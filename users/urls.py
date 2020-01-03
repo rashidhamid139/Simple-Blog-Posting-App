f0d2baa64d51a7d797ca39e06bfc65b9245f8bea
rashidhamid139 <rashidhamid139@gmail.com>
2020-01-03 18:41:39 +0530

    This is my Second commit in which i have enhanced my web app with user login/Authentication

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('', views.register, name='users-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
