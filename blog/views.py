0067778aa59c297b2f0ac88f5818869e86d81e45
rashidhamid139 <rashidhamid139@gmail.com>
2020-01-03 10:26:55 +0530

    This is my commit when my code is at  the stage of adding   User registration feature

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+
+
+def home(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context )
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
